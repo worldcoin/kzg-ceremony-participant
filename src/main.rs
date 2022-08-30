64c3781632681cc3a3b420a683a9ac6cda268313
Philipp Sippl <philsippl@gmail.com>
2022-08-31 00:03:56 +0200

    impl g2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use ark_bls12_381::{Fr as ScalarField, G1Affine as GAffine, G1Projective as G};
+use ark_bls12_381::{Fr as ScalarField, G1Affine as G1Affine, G1Projective as G1, G2Affine as G2Affine, G2Projective as G2};
 use ark_ec::{AffineCurve, ProjectiveCurve};
 use ark_ff::Field;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Write};
@@ -10,54 +10,80 @@ use rand::thread_rng;
 
 const DOMAIN_SIZE: u32 = 12; // support up to 2**12=4096 field elements
 
-fn write_file(ptau: Vec<GAffine>, path: &Path) -> Result<()> {
-    let mut compressed_bytes = Vec::new();
-    for t in ptau.into_iter() {
-        t.serialize(&mut compressed_bytes)?;
-    }
-
+/**
+ * Serialize ptau to a file.
+ */
+fn write_ptau_file(ptau_g1: Vec<G1Affine>, ptau_g2: Vec<G2Affine>, path: &Path) -> Result<()> {
     let mut file = std::fs::File::create(path)?;
-    file.write_all(&compressed_bytes)?;
+    for t in ptau_g1.into_iter() {
+        t.serialize(&mut file)?;
+    }
+    for t in ptau_g2.into_iter() {
+        t.serialize(&mut file)?;
+    }
     Ok(())
 }
 
-// use this function to generate the initial ptau setup
+/**
+ * Generate the initial ptau setup.
+ */
 fn generate_initial() -> Result<()> {
     let mut rng = thread_rng();
 
     // g1 generator
-    let g1 = G::rand(&mut rng).into_affine();
+    let g1 = G1::rand(&mut rng).into_affine();
+    let g2 = G2::rand(&mut rng).into_affine();
 
     // secret
     let s = ScalarField::rand(&mut rng);
 
-    let ptau = cfg_into_iter!(0..u32::pow(2, DOMAIN_SIZE) + 1)
+    // s^i*G1
+    let ptau_g1 = cfg_into_iter!(0..u32::pow(2, DOMAIN_SIZE) + 1)
         .map(|i| g1.mul(s.pow([i as u64])).into_affine())
         .collect::<Vec<_>>();
 
-    write_file(ptau, Path::new("setup.ptau"))
+    // s^i*G2
+    let ptau_g2 = cfg_into_iter!(0..u32::pow(2, DOMAIN_SIZE) + 1)
+        .map(|i| g2.mul(s.pow([i as u64])).into_affine())
+        .collect::<Vec<_>>();
+
+    // serialize and write to file
+    write_ptau_file(ptau_g1, ptau_g2, Path::new("setup.ptau"))
 }
 
-// use this function to apply a user's contibution to the setup
+/**
+ * Apply a user's contibution to the setup
+ */
 fn contribute() -> Result<()> {
     let mut rng = thread_rng();
-    let mut ptau: Vec<GAffine> = Vec::new();
-
+    
     // read ptau file
+    let mut ptau_g1: Vec<G1Affine> = Vec::new();
+    let mut ptau_g2: Vec<G2Affine> = Vec::new();
+
     let mut reader = std::fs::File::open("setup.ptau")?;
     for _ in 0..u32::pow(2, DOMAIN_SIZE) + 1 {
-        ptau.push(GAffine::deserialize(&mut reader)?);
+        ptau_g1.push(G1Affine::deserialize(&mut reader)?);
+    }
+
+    for _ in 0..u32::pow(2, DOMAIN_SIZE) + 1 {
+        ptau_g2.push(G2Affine::deserialize(&mut reader)?);
     }
 
     // private contribution
     let t = ScalarField::rand(&mut rng);
 
     // apply to existing ptau
-    let ptau_contributed = cfg_into_iter!(ptau).enumerate()
+    let ptau_g1_contributed = cfg_into_iter!(ptau_g1).enumerate()
         .map(|(i, sg)| sg.mul(t.pow([i as u64])).into_affine())
         .collect::<Vec<_>>();
 
-    write_file(ptau_contributed, Path::new("contribution.ptau"))
+    let ptau_g2_contributed = cfg_into_iter!(ptau_g2).enumerate()
+        .map(|(i, sg)| sg.mul(t.pow([i as u64])).into_affine())
+        .collect::<Vec<_>>();
+
+    // serialize and write to file
+    write_ptau_file(ptau_g1_contributed, ptau_g2_contributed, Path::new("contribution.ptau"))
 }
 
 fn verify() {
@@ -66,4 +92,5 @@ fn verify() {
 
 fn main() {
     generate_initial().unwrap();
+    contribute().unwrap();
 }
